c472d57ba04f2b549119483ce1f62528061e436f
Frank Puertas <frankgzmc@gmail.com>
2025-07-02 14:46:40 -0500

    Add media configuration on settings.py

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -117,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = '/app/static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/app/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
