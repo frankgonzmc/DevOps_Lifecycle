3bfa0135596ef9915975b1fe82c63ea01aabfb80
Frank Puertas <frankgzmc@gmail.com>
2025-07-28 16:00:36 -0500

    Add outputs on terraform

---
+++ terraform/modules/vpc/output.tf
@@ -0,0 +1,11 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "subnet_public_id" {
+  value = aws_subnet.subnet_public.id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.subnet_private[*].id
+}
